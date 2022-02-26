2ed17389da3e6f874ecd9c3be0da648dff6194ab
chawlajay <chawlajay111@gmail.com>
2022-02-26 20:34:03 +0530

    fetch quote from API, replace text in readme

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const path = require("path");
 
 async function main() {
     const readmeTemplate = (
-        await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+        await fs.readFile(__dirname + "/README.template.md")
     ).toString("utf-8");
     
+    var url = "https://type.fit/api/quotes";
+    let all_quotes = await (await fetch(url)).json();
+    let current_quote = all_quotes[Math.floor(Math.random()*all_quotes.length)];
+
+    const readme = readmeTemplate
+        .replace("{inspirational_quote}", current_quote.text)
+        .replace("{inspirational_author}", `- ${current_quote.author}`)
+
+    await fs.writeFile("README.md", readme);
+    console.log(current_quote.text);
 }
 main();

2b41287dec652a0a9fa436127f6d9fd9cbe041ce
chawlajay <chawlajay111@gmail.com>
2022-02-26 20:24:25 +0530

    add index file to fetch readme-template code

---
+++ index.js
@@ -0,0 +1,11 @@
+require("isomorphic-unfetch");
+const { promises: fs } = require("fs");
+const path = require("path");
+
+async function main() {
+    const readmeTemplate = (
+        await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+    ).toString("utf-8");
+    
+}
+main();
